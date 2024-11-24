bc7b7f969c1484e81377e03afacb7273f148df64
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2024-11-24 12:41:40 +0000

    Add gamecontroller as IIFE

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // gameboard as an array inside of a Gameboard object
 
-
 const gameBoard = (function () {
     const printBoard = {
     board: [ "A1", "B1", "C1", 
@@ -19,27 +18,12 @@ const gameBoard = (function () {
 
     7: ["A1", "B2", "C3" ],
     8: ["A3", "B2", "C1" ]}
-    console.log(winningBoards)
+    
     
     return printBoard, winningBoards
 })(); 
 
 console.log(gameBoard.printBoard)
-
-
-
-// const winningBoards = (function () {
-//     1: ["A1", "B1", "C1" ],
-//     2: ["A2", "B2", "C2" ],
-//     3: ["A3", "B3", "C3" ],
-
-//     4: ["A1", "A2", "A3" ],
-//     5: ["B1", "B2", "B3" ],
-//     6: ["C1", "C2", "C1" ],
-
-//     7: ["A1", "B2", "C3" ],
-//     8: ["A3", "B2", "C1" ],
-// })();
 
 function createPlayer(name, mark) {
     let turn = "";
@@ -57,37 +41,35 @@ const playerTwo = createPlayer("Monika", "O")
 
 // an object to control the flow of the game itself.
 
-function playerChoice() {
-    return prompt("Player 1 turn");
-}
-
-function isSquareTakenPlayerOne(square) {
-    return playerOne.chosenSquares.includes(square)
-}
-
-function isSquareTakenPlayerTwo(square) {
-    return playerTwo.chosenSquares.includes(square)
-}
-
-function gameController() {
-
+const gameController = (function () {
 
     // a player will choose a square - by typing in console first
-    let turn = playerChoice();
 
-    // if square is available (check both players array), add to player array
-    if (!(isSquareTakenPlayerOne(turn) || isSqaureTakenPlayerOne(turn))) {
-        playerOne.chosenSquares.push(turn);
-        console.log(playerOne.chosenSquares);
+    function playerChoice() {
+    return prompt("Player 1 turn");
     }
+
+    let choice = playerChoice();
+
+    // if square is available (check both players array), add to player array & change board
+
+    function isSquareTakenPlayerOne(square) {
+    return playerOne.chosenSquares.includes(square)
+    }
+
+    function isSquareTakenPlayerTwo(square) {
+    return playerTwo.chosenSquares.includes(square)
+    }
+
+    if (!(isSquareTakenPlayerOne(choice) || isSquareTakenPlayerOne(choice))) {
+        playerOne.chosenSquares.push(choice);
+        console.log(playerOne.chosenSquares);
+
+    }
+
+
     // if player array matches win, end game as win
 
     // if no more board, end game as tie
     // if not next player turn until end game
-} 
-
-//gameController();
-
-// Pseudocode 
-
-
+})()
